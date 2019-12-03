85be0e6b2d4a10904e5e24e06fb27072cecea927
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-12-03 22:54:12 +0100

    Register services with Swinject

--- Introspect/ViewController.swift
+++ Introspect/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    init () {
+        super.init(nibName: nil, bundle: nil)
+    }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .red
     }
 }

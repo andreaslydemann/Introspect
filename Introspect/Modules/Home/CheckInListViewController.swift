bb8e709870b62fe1e7d8d965442620f39c277789
Andreas Lüdemann <anld@danskebank.dk>
2019-12-19 06:30:17 +0100

    Clean up in CheckInListViewController

--- Introspect/Modules/Home/CheckInListViewController.swift
+++ Introspect/Modules/Home/CheckInListViewController.swift
@@ -21,17 +21,17 @@ struct CheckIn {
 final class CheckInListViewController: UIViewController {
     weak var delegate: CheckInListViewControllerDelegate?
     private var checkIns: [CheckIn] = []
-    
+
     convenience init(checkIns: [CheckIn]) {
         self.init()
         self.checkIns = checkIns
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
-    
+
     lazy var collectionViewLayout: UICollectionViewLayout = {
         var section = self.section
         let layout = UICollectionViewCompositionalLayout { (_, _) -> NSCollectionLayoutSection? in
@@ -39,28 +39,28 @@ final class CheckInListViewController: UIViewController {
         }
         return layout
     }()
-    
+
     lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
         collectionView.backgroundColor = .systemBackground
         collectionView.delaysContentTouches = false
-        
+
         collectionView.dataSource = self
         collectionView.delegate = self
-        
+
         collectionView.register(CreateCheckInCell.self)
         collectionView.register(CompletedCheckInCell.self)
-        
+
         return collectionView
     }()
-    
-    let section = CheckInSection()
-    
+
+    lazy var section: Section = CheckInSection(checkIns: checkIns)
+
     func setupViews() {
         view.addSubview(collectionView)
         collectionView.fillSuperview()
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         collectionView.reloadData()
@@ -69,40 +69,16 @@ final class CheckInListViewController: UIViewController {
 
 extension CheckInListViewController: UICollectionViewDataSource {
     func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
-        if checkIns.isEmpty { return 1 }
-        return checkIns.count + (checkIns.first!.date.compare(.isToday) ? 0 : 1)
+        return self.section.numberOfItems
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let firstCheckIn = checkIns.first, !firstCheckIn.date.compare(.isToday) && indexPath.row == 0 {
-            return collectionView.dequeueReusable(withCell: CreateCheckInCell.self, for: indexPath)
-        } else {
-            return collectionView.dequeueReusable(withCell: CompletedCheckInCell.self, for: indexPath)
-        }
+        return section.configureCell(collectionView: collectionView, indexPath: indexPath)
     }
-    
+
     func collectionView(_: UICollectionView, didSelectItemAt _: IndexPath) {
         delegate?.didSelectCheckIn("my user id", viewController: self)
     }
 }
 
 extension CheckInListViewController: UICollectionViewDelegate {}
-
-struct CheckInSection {
-    func layoutSection() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.94), heightDimension: .fractionalHeight(1))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPagingCentered
-        
-        return section
-    }
-    
-    func configureCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusable(withCell: CreateCheckInCell.self, for: indexPath)
-    }
-}
---
+++ Introspect/Modules/Home/CheckInSection.swift
@@ -0,0 +1,50 @@
+//
+//  CheckInSection.swift
+//  Introspect
+//
+//  Created by Andreas Lüdemann on 19/12/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import UI
+import UIKit
+
+protocol Section {
+    var numberOfItems: Int { get }
+    func layoutSection() -> NSCollectionLayoutSection
+    func configureCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell
+}
+
+struct CheckInSection: Section {
+    var numberOfItems: Int {
+        if checkIns.isEmpty { return 1 }
+        return checkIns.count + (checkIns.first!.date.compare(.isToday) ? 0 : 1)
+    }
+
+    private let checkIns: [CheckIn]
+
+    init(checkIns: [CheckIn]) {
+        self.checkIns = checkIns
+    }
+
+    func layoutSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.94), heightDimension: .fractionalHeight(1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+
+        return section
+    }
+
+    func configureCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+        if let firstCheckIn = checkIns.first, !firstCheckIn.date.compare(.isToday) && indexPath.row == 0 {
+            return collectionView.dequeueReusable(withCell: CreateCheckInCell.self, for: indexPath)
+        } else {
+            return collectionView.dequeueReusable(withCell: CompletedCheckInCell.self, for: indexPath)
+        }
+    }
+}
